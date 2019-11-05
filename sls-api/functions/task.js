36e6a4481572f3f9acc9499d7cceadff7e9dbd72
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-11-05 16:39:28 +0900

    Fix: task周りのCRUD機能の修正

--- sls-api/functions/task.js
+++ sls-api/functions/task.js
@@ -2,17 +2,23 @@ import uuid from 'uuid'
 import * as dynamoDbLib from '../libs/dynamodb-lib'
 import { success, failure } from '../libs/response-lib'
 
+const TableName = `Todo_${process.env.DEPLOY_STAGE}`
+
 export const createTask = async event => {
   const params = {
-    TableName: `Todo_${process.env.DEPLOY_STAGE}`,
+    TableName: TableName,
     Item: {
-      taskId: uuid.v4(),
-      title: event.body.title,
-      description: event.body.description,
-      list: event.body.list,
-      userId: event.requestContext.identity.cognitoIdentityId,
+      group: 'task',
+      groupId: `task_${uuid.v4()}`,
+      ownerId: event.requestContext.identity.cognitoIdentityId,
       createdAt: Date.now(),
-      updatedAt: Date.now()
+      updatedAt: Date.now(),
+      attributes: {
+        title: event.body.title,
+        description: event.body.description,
+        list: event.body.list,
+        priority: event.body.priority
+      }
     }
   }
 
@@ -26,10 +32,10 @@ export const createTask = async event => {
 
 export const deleteTask = async event => {
   const params = {
-    TableName: `Todo_${process.env.DEPLOY_STAGE}`,
+    TableName: TableName,
     Key: {
-      taskId: event.pathParameters.taskId,
-      userId: event.requestContext.identity.cognitoIdentityId
+      group: 'task',
+      groupId: `task_${event.pathParameters.taskId}`
     }
   }
 
@@ -43,10 +49,10 @@ export const deleteTask = async event => {
 
 export const getTask = async event => {
   const params = {
-    TableName: `Todo_${process.env.DEPLOY_STAGE}`,
+    TableName: TableName,
     Key: {
-      taskId: event.pathParameters.taskId,
-      userId: event.requestContext.identity.cognitoIdentityId
+      group: 'task',
+      groupId: `task_${event.pathParameters.taskId}`
     }
   }
 
@@ -64,30 +70,58 @@ export const getTask = async event => {
 
 export const updateTask = async event => {
   const params = {
-    TableName: `Todo_${process.env.DEPLOY_STAGE}`,
+    TableName: TableName,
     Key: {
-      taskId: event.pathParameters.taskId,
-      userId: event.requestContext.identity.cognitoIdentityId
+      group: 'task',
+      groupId: `task_${event.pathParameters.taskId}`
     },
     ExpressionAttributeNames: {
-      '#t': 'title',
-      '#d': 'description',
-      '#l': 'list',
+      '#a': 'attributes',
       '#uA': 'updatedAt'
     },
     ExpressionAttributeValues: {
-      ':title': event.body.title || '',
-      ':description': event.body.description || '',
-      ':list': event.body.list || 'backlog',
+      ':attributes': {
+        title: event.body.title || '',
+        description: event.body.description || '',
+        list: event.body.list || 'backlog',
+        priority: event.body.priority || 0
+      },
       ':updatedAt': Date.now()
     },
-    UpdateExpression: 'SET #t = :title, #d = :description, #l = :list, #uA = :updatedAt',
+    UpdateExpression: 'SET #a = :attributes, #uA = :updatedAt',
     ReturnValues: 'ALL_NEW'
   }
 
   try {
     await dynamoDbLib.call('update', params)
     return success({ status: true })
+  } catch (e) {
+    return failure(e)
+  }
+}
+
+export const findTasks = async event => {
+  const params = {
+    TableName: TableName,
+    IndexName: 'group-ownerId-index',
+    ExpressionAttributeNames: {
+      '#group': 'group',
+      '#ownerId': 'ownerId'
+    },
+    ExpressionAttributeValues: {
+      ':group': 'task',
+      ':ownerId': event.requestContext.identity.cognitoIdentityId
+    },
+    KeyConditionExpression: '#group = :group AND #ownerId = :ownerId'
+  }
+
+  try {
+    const result = await dynamoDbLib.call('query', params)
+    if (result.Count) {
+      return success(result.Items)
+    } else {
+      return failure({ status: false, error: 'Item not found.' })
+    }
   } catch (e) {
     return failure(e)
   }
