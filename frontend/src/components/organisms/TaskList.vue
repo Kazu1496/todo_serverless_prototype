35cf916ed5b27d96c8a923c6727b889e0ced5193
Kazumasa Ishikura <kazumasa1496@gmail.com>
2019-11-20 11:51:12 +0900

    Fix: length undefindバグ修正

--- frontend/src/components/organisms/TaskList.vue
+++ frontend/src/components/organisms/TaskList.vue
@@ -4,7 +4,7 @@
       p.label {{ label | formatListName }}
       button(v-if="label !== 'DONE'" @click="$emit('show', label)")
         font-awesome-icon(icon="plus")
-      button.purge(v-else @click="purgeTasks" :disabled="sortedTasks.length === 0") Purge
+      button.purge(v-else @click="purgeTasks") Purge
     draggable(
       v-if="sortedTasks.length"
       tag="ul" group="TASKS"
